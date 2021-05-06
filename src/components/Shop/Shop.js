049c0b5c009555c4f40c6104bb21bb7b6c671d83
jonathan-sheets <70708744+jonathan-sheets@users.noreply.github.com>
2021-05-06 10:05:44 -0600

    added all elements including spreadshirt store to component

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,14 +1,64 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import NavBar from '../NavBar/NavBar';
+import './Shop.css';
 
 class Shop extends React.Component {
   render() {
+    let js = document.createElement("script");
+    js.type = "text/javascript";
+    js.src = "https://shop.spreadshirt.com/shopfiles/shopclient/shopclient.nocache.js"
+    document.body.appendChild(js);
+
     return (
-      <div className="shop">
+      <div id="shop">
       <>
       <NavBar />
       </>
-        <p>Spreadshirt store goes here</p>
+
+        <Container fluid className="shop">
+          <Container className="content">
+            <Row>
+              <Col id="myShop">
+
+              </Col>
+            </Row>
+          </Container>
+          <Row>
+            <Col>
+              <div className="donate">
+                <p>If you like our music and want to hear more, consider being a hero and donate to the cause.
+                   All donations go through PayPal and help us cover licensing and distribution costs.</p>
+              </div>
+              <div className="donate-action">
+                <i className="fa fa-paypal fa-2x paypal" aria-hidden="true"></i>
+                <form action="https://www.paypal.com/donate" method="post" target="_blank">
+                  <input 
+                    type="hidden" 
+                    name="hosted_button_id" 
+                    value="6VMA89ES6AHCN"   
+                  />
+                  <input 
+                    className="donate-button"
+                    type="submit" 
+                    value="Make a Donation"
+                    // src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" 
+                    border="0" 
+                    name="submit" 
+                    title="PayPal - The safer, easier way to pay online!" 
+                    alt="Donate with PayPal button" 
+                  />
+                  <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+                </form>
+                <i className="fa fa-paypal fa-2x paypal" aria-hidden="true"></i>
+              </div> 
+              
+            </Col>
+          </Row>
+        </Container>
+
       </div>
     )
   }
