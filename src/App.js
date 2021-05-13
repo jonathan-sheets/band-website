1adfc3ab8afc009cba29315067b33b08b14aafb3
jonathan-sheets <70708744+jonathan-sheets@users.noreply.github.com>
2021-05-13 13:37:32 -0600

    updated routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Music from './components/Music/Music';
 import Photos from './components/Photos/Photos';
 import Blog from './components/Blog/Blog';
+import Article from './components/Blog/Article';
 import Video from './components/Video/Video';
 import Shop from './components/Shop/Shop';
 import './App.css';
@@ -14,15 +15,16 @@ export default function App() {
     <div className="App">
       <div>
         <BrowserRouter>
-      
+        <Switch>
           <Route component={Home} path="/" exact />
           <Route component={About} path="/about" />
           <Route component={Music} path="/music" />
           <Route component={Photos} path="/photos" />
           <Route component={Blog} path="/blog" />
+          <Route component={Article} path="/blog/:slug" exact />
           <Route component={Video} path="/video" />
           <Route component={Shop} path="/merch" />
-      
+        </Switch>
         </BrowserRouter>
       </div>
     </div>
