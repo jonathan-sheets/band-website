0e8295bdab428b58b6399211cdc0bae32c0dec06
jonathan-sheets <70708744+jonathan-sheets@users.noreply.github.com>
2021-05-18 14:15:49 -0600

    cleaned up unused code, updated link url to full blog post

--- src/components/Blog/ArticlePreview.js
+++ src/components/Blog/ArticlePreview.js
@@ -23,8 +23,6 @@ class ArticlePreview extends React.Component {
       return (
         <div className="article">
           <h3 className="text-center">{title}</h3>
-          {/* <a href={"/blog/" + this.props.post.ID} className="blackLink"> */}
-          <p>
             {this.props.post.featured_image ? (
               <img
                 className="img-responsive webpic"
@@ -34,13 +32,10 @@ class ArticlePreview extends React.Component {
             ) : (
               ""
             )}
-            
-          </p>  
           <div className="article-content">{excerpt}</div>
-          {/* </a> */}
-          <Link to={`/blog/${this.props.post.slug}`}>
-            <button className="btn">Read More</button>
-          </Link>
+            <Link to={`/blog/${this.props.post.slug}`}>
+              <button className="btn">Read More</button>
+            </Link>
         </div>
       );
     } else {
