3366927c416445b08fa95b797219bd2c79077c15
jonathan-sheets <70708744+jonathan-sheets@users.noreply.github.com>
2021-05-20 11:20:34 -0600

    updated routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/Home/Home';
 import About from './components/About/About';
 import Music from './components/Music/Music';
@@ -8,24 +8,26 @@ import Blog from './components/Blog/Blog';
 import Article from './components/Blog/Article';
 import Video from './components/Video/Video';
 import Shop from './components/Shop/Shop';
+import Donors from './components/Donors/Donors';
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
       <div>
-        <BrowserRouter>
-        <Switch>
-          <Route component={Home} path="/" exact={true} />
-          <Route component={About} path="/about" />
-          <Route component={Music} path="/music" />
-          <Route component={Photos} path="/photos" />
-          <Route component={Blog} path="/blog" exact={true} />
-          <Route component={Article} path="/blog/:slug" exact={true} />
-          <Route component={Video} path="/video" />
-          <Route component={Shop} path="/merch" />
-        </Switch>
-        </BrowserRouter>
+        <Router>
+          <Switch>
+            <Route component={Home} path="/" exact={true} />
+            <Route component={About} path="/about" />
+            <Route component={Music} path="/music" />
+            <Route component={Photos} path="/photos" />
+            <Route component={Blog} path="/blog" exact={true} />
+            <Route component={Article} path="/blog/:slug" exact={true} />
+            <Route component={Video} path="/video" />
+            <Route component={Shop} path="/merch" />
+            <Route component={Donors} path="/donors" />
+          </Switch>
+        </Router>
       </div>
     </div>
   );
